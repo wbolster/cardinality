63c5494b2dd16b3d28aca0120fc9b82218b972a2
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 18:26:44 +0100

    DRY in documentation configuration

--- doc/conf.py
+++ doc/conf.py
@@ -1,11 +1,11 @@
+import cardinality
+
 extensions = [
     'sphinx.ext.autodoc',
 ]
 
-# templates_path = ['_templates']
-# source_suffix = '.rst'
 master_doc = 'index'
 project = 'cardinality'
-copyright = '2015, Wouter Bolsterlee'
-version = '0.0.1'
-release = '0.0.1'
+copyright = 'Wouter Bolsterlee'
+version = cardinality.__version__
+release = cardinality.__version__
