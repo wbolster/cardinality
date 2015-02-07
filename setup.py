ee006cea8f908a8ac5b1feb9f0d16a19345a73c4
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 15:08:04 +0100

    Add basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cardinality',
+    version='0.0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url="https://github.com/wbolster/cardinality",
+    py_modules=['cardinality'],
+)
