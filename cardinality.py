ce24d0ffaff9887f72a72e8162279936bc47b849
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 18:43:23 +0100

    Bump version to 0.1.0

--- cardinality.py
+++ cardinality.py
@@ -7,7 +7,7 @@ cardinality: determine and check the size of any iterable
 import collections
 import itertools
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __version_info__ = tuple(map(int, __version__.split('.')))
 
 _SENTINEL = object()
