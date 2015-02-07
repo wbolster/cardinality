5040adf4ee77b07752ffc619c89368771bfe1461
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 17:41:06 +0100

    Add initial docs (using Sphinx)

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+# templates_path = ['_templates']
+# source_suffix = '.rst'
+master_doc = 'index'
+project = 'cardinality'
+copyright = '2015, Wouter Bolsterlee'
+version = '0.0.1'
+release = '0.0.1'
---
+++ doc/index.rst
@@ -0,0 +1,11 @@
+cardinality
+===========
+
+
+API
+===
+
+.. autofunction:: cardinality.count
+.. autofunction:: cardinality.at_least
+.. autofunction:: cardinality.at_most
+.. autofunction:: cardinality.between
