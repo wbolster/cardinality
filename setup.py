5e280c285346eee4aaa24efa775ed9a1b351846e
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 18:27:21 +0100

    Expand setup script to include more info

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+import os
 from setuptools import setup
+
+import cardinality
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
 
 setup(
     name='cardinality',
-    version='0.0.1',
+    description="Determine and check the size of any iterable",
+    long_description=long_description,
+    version=cardinality.__version__,
     author="Wouter Bolsterlee",
     author_email="uws@xs4all.nl",
     url="https://github.com/wbolster/cardinality",
