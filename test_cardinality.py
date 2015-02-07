fce65b7d3d29be2b5c0a2a7feed1809d23fd0224
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 16:28:20 +0100

    Reorder/rename tests a bit

--- test_cardinality.py
+++ test_cardinality.py
@@ -18,29 +18,15 @@ def test_count():
 
 
 def test_at_least():
-
     assert cardinality.at_least(0, [])
     assert cardinality.at_least(0, [1, 2])
     assert cardinality.at_least(0, generate(0))
     assert cardinality.at_least(0, generate(2))
-
     assert cardinality.at_least(2, [1, 2])
     assert not cardinality.at_least(3, [1, 2])
     assert cardinality.at_least(1, generate(1))
     assert cardinality.at_least(2, generate(4))
     assert not cardinality.at_least(10, generate(2))
-
-
-def test_at_least_invalid_size():
-    with pytest.raises(ValueError) as e:
-        cardinality.at_least(-1, [])
-    assert 'must be positive' in str(e.value)
-
-
-def test_at_least_non_iterable():
-    with pytest.raises(TypeError) as e:
-        cardinality.at_least(0, object())
-    assert 'is not iterable' in str(e.value)
 
 
 def test_at_most():
@@ -49,7 +35,6 @@ def test_at_most():
     assert cardinality.at_most(2, [1])
     assert cardinality.at_most(2, [1, 2])
     assert not cardinality.at_most(2, [1, 2, 3])
-
     assert cardinality.at_most(0, generate(0))
     assert cardinality.at_most(1, generate(0))
     assert cardinality.at_most(1, generate(1))
@@ -63,6 +48,18 @@ def test_between():
     assert cardinality.between(0, 2, [1])
     assert cardinality.between(1, 1, [1])
     assert cardinality.between(1, 1, [1])
+
+
+def test_invalid_size():
+    with pytest.raises(ValueError) as e:
+        cardinality.at_least(-1, [])
+    assert 'must be positive' in str(e.value)
+
+
+def test_non_iterable():
+    with pytest.raises(TypeError) as e:
+        cardinality.at_least(0, object())
+    assert 'is not iterable' in str(e.value)
 
 
 def test_between_invalid_range():
