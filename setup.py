10a4a120c1165ce76c5f75e6b80909a8d5cbc12f
Wouter Bolsterlee <uws@xs4all.nl>
2015-02-07 18:37:06 +0100

    Add license and classifiers to package metadata

--- setup.py
+++ setup.py
@@ -15,4 +15,15 @@ setup(
     author_email="uws@xs4all.nl",
     url="https://github.com/wbolster/cardinality",
     py_modules=['cardinality'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
